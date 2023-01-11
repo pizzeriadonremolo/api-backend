4457002cfebfbcab74f562677872bb9caef10632
alefinir <31045393+alefinir@users.noreply.github.com>
2023-01-10 22:41:53 -0300

    update https

--- config/http.js
+++ config/http.js
@@ -1,6 +1,12 @@
-import App from "./express.js";
-import { createServer } from "http";
 
-const httpServer = createServer(App);
+import App from './express.js';
+import { createServer } from 'https';
+import fs from 'fs';
+
+const httpServer = createServer({
+key: fs.readFileSync('../certs/privkey.pem'),
+cert:fs.readFileSync('../certs/fullchain.pem')
+}
+,App);
 
 export default httpServer;
