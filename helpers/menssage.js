c3c3cf83b01de45ae3414d4ccd16c80280378756
Jonathan Daniel Arce Aguirre <112012487+ArceDaniel@users.noreply.github.com>
2023-02-27 18:17:56 -0300

    Update menssage.js

--- helpers/menssage.js
+++ helpers/menssage.js
@@ -5,6 +5,6 @@ const sendMenssage = (obj)=>{
 
 
     console.log(process.env.PHONE);
-    return `https://api.whatsapp.com/send?phone=${process.env.PHONE}&text=mi%20pedido%20es:%20${process.env.URL_FRONT}/view/${obj.number}`
+    return `https://api.whatsapp.com/send?phone=5491123346010&text=mi%20pedido%20es:%20${process.env.URL_FRONT}/view/${obj.number}`
 }
 export default sendMenssage
